a332a1b717995a6d979548242a996ccda503d3e4
DragaoBranco001 <dbranco460@gmail.com>
2024-12-09 16:21:26 -0300

    Add files via upload

---
+++ KMSPico Windows Key.bat
@@ -0,0 +1,126 @@
+@echo off
+Title KMSPico Windows Key *Dragon White*
+color 4
+echo ###                                   ##
+echo   ##
+echo   ##  ##  ##  ##    #####   ######    ###      ####     ####
+echo   ## ##   #######  ##        ##  ##    ##     ##  ##   ##  ##
+echo   ####    ## # ##   #####    ##  ##    ##     ##       ##  ##
+echo   ## ##   ##   ##       ##   #####     ##     ##  ##   ##  ##
+echo   ##  ##  ##   ##  ######    ##       ####     ####     ####
+echo                             ####
+:menu
+echo Computador: %computername% Usuario: %username%
+echo MENU TAREFAS
+echo ==================================
+echo * 1. Ativar Windows 8 e 8.1 Pro  *
+echo * 2. Ativar Windows 10 e 11 Pro  *
+echo * 3. Ativar Windows 11 Pro       *
+echo * 4. Random Key Windows          *
+echo * 5. Sair                        *
+echo ==================================
+set /p opcao= Escolha uma opcao:
+echo ------------------------------
+if %opcao% equ 1 goto opcao1
+if %opcao% equ 2 goto opcao2
+if %opcao% equ 3 goto opcao3
+if %opcao% equ 4 goto opcao4
+if %opcao% equ 5 goto opcao5
+if %opcao% GEQ 6 goto opcao6
+:opcao1
+cls
+echo Ativando Windows 8 e 8.1.
+slmgr /ipk GCRJD-8NW9H-F2CDX-CCM8D-9D6T9
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo ==================================
+echo * Processo Concluido *
+echo ==================================
+pause
+cls
+goto menu
+:opcao2
+cls
+echo echo Ativando Windows 10 e 11 Pro.
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo ==================================
+echo * Processo Concluido *
+echo ==================================
+pause
+cls
+goto menu
+:opcao3
+cls
+echo ==================================
+echo * Processo Concluido *
+echo ==================================
+echo Ativando Windows 11 Pro.
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+slmgr /ato
+pause
+cls
+goto menu
+:opcao4
+cls
+echo Ativando Windows Random Mode.
+slmgr /ipk VK7JG-NPHTM-C97JM-9MPGT-3V66T
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Funcionou Feche no X. Nao Funcionou continue...
+pause
+cls
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Funcionou Feche no X. Nao Funcionou continue...
+pause
+cls
+slmgr /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Funcionou Feche no X. Nao Funcionou continue...
+pause
+cls
+slmgr /ipk 2B87N-8KFHP-DKV6R-Y2C8J-PKCKT
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Funcionou Feche no X. Nao Funcionou continue...
+pause
+cls
+slmgr /ipk ZK7JG-NPHTM-C97JM-9MPGT-3V66T
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Funcionou Feche no X. Nao Funcionou continue...
+pause
+cls
+slmgr /ipk Q269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Funcionou Feche no X. Nao Funcionou continue...
+pause
+cls
+slmgr /ipk MRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Funcionou Feche no X. Nao Funcionou continue...
+pause
+cls
+slmgr /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Funcionou Feche no X. Nao Funcionou continue...
+pause
+cls
+goto menu
+:opcao5
+cls
+exit
+:opcao6
+echo ==============================================
+echo * Opcao Invalida! Escolha outra opcao do menu *
+echo ==============================================
+pause
+goto menu
